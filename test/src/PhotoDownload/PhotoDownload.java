0d554f5daf263517266ff1436f6b79508c8c44dd
wangyuefive <wangyuefive@163.com>
2014-08-02 16:31:03 +0800

    修改

--- test/src/PhotoDownload/PhotoDownload.java
+++ test/src/PhotoDownload/PhotoDownload.java
@@ -1,5 +1,7 @@
 package PhotoDownload;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,6 +15,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -29,7 +32,7 @@ public class PhotoDownload extends JFrame{
 	private JTextField urlText = new JTextField();
 	
 	private JFileChooser docChooser = new JFileChooser(new File(System.getProperty("user.dir")));
-	
+	private JCheckBox box_all = new JCheckBox("全部");
 	private JCheckBox box_png = new JCheckBox("png");
 	private JCheckBox box_jpg = new JCheckBox("jpg");
 	private JCheckBox box_bmp = new JCheckBox("bmp");
@@ -52,18 +55,59 @@ public class PhotoDownload extends JFrame{
 		this.setResizable(false);
 		
 		JPanel pnl = new JPanel();
+		pnl.setLayout(new BoxLayout(pnl, BoxLayout.Y_AXIS));
 		
 		JLabel lbl_url = new JLabel("网址：");
+		lbl_url.setSize(20,100);
 		Box hbox1 = Box.createHorizontalBox();
+		urlText.setSize(20,300);
 		hbox1.add(lbl_url);
 		hbox1.add(urlText);
 		
+		Box hbox2 = Box.createHorizontalBox();
 		JLabel lbl_photoType = new JLabel("图片格式");
-		JCheckBox box_png = new JCheckBox("png");
-		JCheckBox box_jpg = new JCheckBox("jpg");
-		JCheckBox box_bmp = new JCheckBox("bmp");
-		JCheckBox box_gif = new JCheckBox("gif");
+		lbl_photoType.setSize(20,100);
+		hbox2.add(box_jpg);
+		hbox2.add(box_gif);
+		hbox2.add(box_bmp);
+		hbox2.add(box_png);
+		hbox2.add(box_all);
+		box_bmp.addActionListener(boxlistener);
+		box_jpg.addActionListener(boxlistener);
+		box_gif.addActionListener(boxlistener);
+		box_png.addActionListener(boxlistener);
+		box_all.addActionListener(boxAllListener);
+		hbox2.add(lbl_photoType);
+		hbox2.add(box_jpg);
+		hbox2.add(box_gif);
+		hbox2.add(box_bmp);
+		hbox2.add(box_png);
+		hbox2.add(box_all);
+		
+		Box hbox3 = Box.createHorizontalBox();
+		JLabel labl_sizelimit = new JLabel("图片大小：");
+		
+		
 	}
+	
+	private ActionListener boxAllListener = new ActionListener() {
+		
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			box_png.setSelected(false);
+			box_jpg.setSelected(false);
+			box_gif.setSelected(false);
+			box_bmp.setSelected(false);
+		}
+	};
+	
+	private ActionListener boxlistener = new ActionListener() {
+		
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			box_all.setSelected(false);
+		}
+	};
 	
 	/**
 	 * URL文档解析，返回网页
@@ -87,20 +131,34 @@ public class PhotoDownload extends JFrame{
 		return null;
 	}
 	
-	
-	
 	/**
-	 * 生成Regex正则表达式
+	 * 根据界面选择生成Regex正则表达式
 	 * @return
 	 */
 	private String createRegex(){
-		String regex = null;
-		if(box_bmp.isSelected())
-			regex = 
+		String regex = "";
+		ArrayList<String> list = new ArrayList<String>();
+		if(box_all.isSelected()){  //全选
+			regex = jpg_regex + "|" +gif_regex +"|" + bmp_regex+"|" + png_regex;
+		}
+		else{
+			if(box_jpg.isSelected())
+				list.add(jpg_regex);
+			if(box_bmp.isSelected())
+				list.add(bmp_regex);
+			if(box_png.isSelected())
+				list.add(png_regex);
+			if(box_gif.isSelected())
+				list.add(gif_regex);
+			for(int i =0 ; i < list.size(); i++){
+				regex = list.get(i) + "|";
+			}
+			regex = regex.substring(0,regex.length()-1);  //去掉最后一个 “|”
+		}
+		return regex;
 	}
 	
-	
-	
-	
-
+	public static void main(String[] args){
+		
+	}
 }
---
+++ test/src/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test {
+	public static void main(String[] args){
+		String str = "0123456789";
+		str = str.substring(0,str.length()-1);
+		System.out.println(str);
+	}
+
+}
